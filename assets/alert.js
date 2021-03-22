0fac81d8fdc29c86376e2e27808007ba8873b6fa
Grisha <grigorije05@gmail.com>
2021-03-22 18:31:00 +0100

    Multiple alerts fix

--- assets/alert.js
+++ assets/alert.js
@@ -1,5 +1,8 @@
+var alerts = 0;
+
 function hideAlertMessage() {
-    var alert_node = document.getElementsByClassName("alert")[0];
+    var nodes = document.getElementsByClassName("alert")
+    var alert_node = nodes[nodes.length-1];
     alert_node.style.opacity = "0";
     window.setTimeout(function() {
         document.body.removeChild(alert_node);
@@ -7,11 +10,14 @@ function hideAlertMessage() {
 }
 
 function showAlertMessage(message) {
+    var my_id = alerts.toString();
+    alerts += 1;
     var temp = document.getElementsByClassName("alert-template")[0];
     var alert_frag = temp.content.cloneNode(true);
-    document.body.appendChild(alert_frag);
-    var alert_node = document.getElementsByClassName("alert")[0];
+    var alert_node = alert_frag.querySelector(".alert");
+    alert_node.id = my_id;
     alert_node.querySelector("h4").innerHTML = message
+    document.body.appendChild(alert_frag);
     window.setTimeout(function() {
         alert_node.style.opacity = 1;
     }, 100);
